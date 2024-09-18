b51370cce42bb48fc6b07e173536bf10d86dde44
warnetmot <reyfernandojuarez@gmail.com>
2024-09-18 13:55:31 -0400

    Prueba en react con la biblioteca de material UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Container } from "@mui/material";
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return(
+      <>
+      <h1> Tecnologia web I  </h1>
+      <Button variant="contained"> button </Button>
+      </>
+  );
+}
